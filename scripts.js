df38ba209cf535d4afb567279790372a4363defd
zemownkee <zemownkee@gmail.com>
2022-10-14 22:36:22 -0400

    Initialize Project
    Create html and js docs
    Sketch out pseudocode'

---
+++ scripts.js
@@ -0,0 +1,17 @@
+//define a function that plays one round
+function playRound() {
+
+    //get choices from computer and player
+
+    //logical comparisons with a variable for player victory
+
+}
+//define a function that plays multiple rounds
+function game() {
+
+    //define variable for win count
+
+    //for loop    
+
+        //call playRound
+}
