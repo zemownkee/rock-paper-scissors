33600eaa2380d39dcf85fb79ab14ae7ae5a9de1f
zemownkee <zemownkee@gmail.com>
2022-11-06 19:37:23 -0500

    Remove unneeded reference file

--- ui-scripts.js
+++
@@ -1,8 +0,0 @@
-
-//return ID of clicked element
-function getPlayerChoice() {
-const targets = document.querySelectorAll('.choice');
-targets.forEach((target) => target.addEventListener('click', () =>{
-    console.log(target.id);
-}));
-}
